711033372b73204065858637e96ec49155052ef7
tartom7997 <tartom7997@gmail.com>
2021-11-02 11:48:29 +0900

    first commit

---
+++ app/javascript/packs/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
+import App from '../components/App'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>,
+    // ID root 要素を取得
+    document.querySelector('#root'),
+  );
+});
+
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import PropTypes from 'prop-types'
+
+// const Hello = props => (
+//   <div>Hello {props.name}!</div>
+// )
+
+// Hello.defaultProps = {
+//   name: 'David'
+// }
+
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <Hello name="React" />,
+//     document.body.appendChild(document.createElement('div')),
+//   )
+// })
